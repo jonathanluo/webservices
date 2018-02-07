be197cfc1ee70e290bd1d4a282f6561f941b12a4
moonwave <moonwave.systems@gmail.com>
2018-02-06 22:53:37 -0800

    query params

--- rest/tpoint/src/com/javatpoint/restclient/ClientTest.java
+++ rest/tpoint/src/com/javatpoint/restclient/ClientTest.java
@@ -1,6 +1,11 @@
 package com.javatpoint.restclient;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -29,10 +34,63 @@ public class ClientTest {
         // token authentication
         System.out.println(target.path("rest").path("hello").request().header("Authorization", "Token " + ACCESS_TOKEN)
                 .accept(MediaType.TEXT_HTML).get(String.class));
+
+        System.out.println(query("rest/hello"));
+        System.out.println(query("rest/hello", "limit=100&pageSize=10"));
+    }
+
+    private static String query(String path) {
+        ClientConfig config = new ClientConfig();
+        Client client = ClientBuilder.newClient(config);
+        WebTarget target = client.target(getBaseURI());
+        target.path(path).request().accept(MediaType.APPLICATION_JSON).get(String.class);
+        target.path(path).request().accept(MediaType.TEXT_PLAIN).get(String.class);
+        
+        // token authentication
+        String responseStr = target.path(path).request().header("Authorization", "Token " + ACCESS_TOKEN)
+                .accept(MediaType.APPLICATION_JSON).get(String.class);
+        System.out.println(responseStr);
+        return responseStr;
+    }
+
+    private static String query(String path, String queryParams) {
+        ClientConfig config = new ClientConfig();
+        Client client = ClientBuilder.newClient(config);
+        WebTarget target = client.target(getBaseURI());
+        target = target.path(path);
+
+        Map<String, String> map = getQueryMap(queryParams);
+        Iterator<Entry<String, String>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<String, String> entry = it.next();
+            target = target.queryParam(entry.getKey(), entry.getValue());
+        }
+
+        // token authentication
+        String responseStr = target.request().header("Authorization", "Token " + ACCESS_TOKEN)
+                .accept(MediaType.APPLICATION_JSON).get(String.class);
+        System.out.println(responseStr);
+        return responseStr;
     }
 
     private static URI getBaseURI() {
         //here server is running on 4444 port number and project name is restfuljersey
         return UriBuilder.fromUri("http://localhost:4444/restfuljersey").build();
     }
-}
+
+    /**
+     * https://coderanch.com/t/383310/java/parse-url-query-string-parameter
+     * @param queryParams e.g. limit=100&pageSize=10
+     * @return
+     */
+    private static Map<String, String> getQueryMap(String queryParams) {
+        String[] params = queryParams.split("&");
+        Map<String, String> map = new HashMap<String, String>();
+        for (String param : params)
+        {
+            String name = param.split("=")[0];
+            String value = param.split("=")[1];
+            map.put(name, value);
+        }
+        return map;
+    }}
