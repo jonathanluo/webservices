08beb5531a7a5052c4a90d081e58627ec8878b50
drachenrio <moonwave.systems@gmail.com>
2018-03-04 23:52:42 -0800

    file upload data in db

--- rest/jersey-rest-client/src/com/fileupload/UploadFileTest.java
+++ rest/jersey-rest-client/src/com/fileupload/UploadFileTest.java
@@ -42,4 +42,24 @@ public class UploadFileTest {
 
 /*
 201 Created InboundJaxrsResponse{context=ClientResponse{method=POST, uri=http://localhost:8000/file/upload/, status=201, reason=Created}}
+
+open ~/workspace/github/jon/webservices/rest/jersey-rest-client/db.sqlite3
+
+table file_app_file 
+
+id    file                          remark                 timestamp
+"1"  "20180210_135850.jpg"         "test file upload"     "2018-02-23 06:45:00.327620"
+"2"  "track-and-field.jpg"         "test file upload #2"  "2018-03-05 05:37:35.427048"
+"3"  "track-and-field.jpg"         "Picture to be upload" "2018-03-05 06:21:54.347663"
+"4"  "track-and-field.jpg"         "Picture to be upload" "2018-03-05 06:26:01.095827"
+"5"  "pydev-run-1.png"             "Picture to be upload" "2018-03-05 06:26:01.294001"
+"6"  "pydev-run-2.png"             "Picture to be upload" "2018-03-05 06:26:01.414246"
+"7"  "track-and-field_ClefvLi.jpg" "Picture to be upload" "2018-03-05 06:39:42.721089"
+"8"  "pydev-run-1_dZ6WiHu.png"     "Picture to be upload" "2018-03-05 06:39:43.001307"
+"9"  "pydev-run-2_VMNXQRb.png"     "Picture to be upload" "2018-03-05 06:39:43.233302"
+"10" "track-and-field.jpg"         "Picture to be upload" "2018-03-05 06:40:17.223578"
+"11" "pydev-run-1.png"             "Picture to be upload" "2018-03-05 06:40:17.431705"
+"12" "pydev-run-2.png"             "Picture to be upload" "2018-03-05 06:40:17.596441"
+"13" "track-and-field_tTPwQw3.jpg" "core image"           "2018-03-05 07:25:48.654359"
+
 */
